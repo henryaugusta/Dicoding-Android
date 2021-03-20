850a9e6ccc27cffc91d6c66622256ef9ff4fd763
henryaugusta <henryaugusta4@gmail.com>
2021-03-20 15:46:14 +0700

    add retrieve notation code

---
+++ NavigationDrawer/app/src/main/java/com/feylabs/navigationdrawer/tab_layout/MyTabLayout.kt
@@ -0,0 +1,12 @@
+package com.feylabs.navigationdrawer.tab_layout
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.feylabs.navigationdrawer.R
+
+class MyTabLayout : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_my_tab_layout)
+    }
+}
