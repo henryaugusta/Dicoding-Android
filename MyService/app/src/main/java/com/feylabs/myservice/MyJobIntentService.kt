b62860be97933e350a20e3ddb715d0ed2e4388e0
henryaugusta <henryaugusta4@gmail.com>
2021-04-04 23:58:27 +0700

    __small changes at Background Service and Broadcast Receiver

--- MyService/app/src/main/java/com/feylabs/myservice/MyJobIntentService.kt
+++ MyService/app/src/main/java/com/feylabs/myservice/MyJobIntentService.kt
@@ -6,40 +6,49 @@ import android.content.Intent
 import android.util.Log
 import androidx.core.app.JobIntentService
 
-// TODO: Rename actions, choose action names that describe tasks that this
-// IntentService can perform, e.g. ACTION_FETCH_NEW_ITEMS
-const val ACTION_FOO = "com.feylabs.myservice.action.FOO"
-const val ACTION_BAZ = "com.feylabs.myservice.action.BAZ"
-
-// TODO: Rename parameters
-const val EXTRA_PARAM1 = "com.feylabs.myservice.extra.PARAM1"
-const val EXTRA_PARAM2 = "com.feylabs.myservice.extra.PARAM2"
-
-/**
- * An [IntentService] subclass for handling asynchronous task requests in
- * a service on a separate handler thread.
- * TODO: Customize class - update intent actions and extra parameters.
- */
 class MyJobIntentService : JobIntentService() {
+
+    var command = "Default Command"
 
     companion object{
         private const val JOB_ID = 1000
         internal const val EXTRA_DURATION = "extra_duration"
+        internal const val COMMAND = "extra_dSSuration"
+        internal const val COMMAND_INTENT = "ZOMCS"
+        internal const val BROADCAST_COMMAND_STATUS = "ZOMxCS"
         private val TAG = MyJobIntentService::class.java.simpleName
 
         fun setEnqueueWork(context: Context,intent: Intent){
             //register enqueue work
-            enqueueWork(context,MyJobIntentService::class.java, JOB_ID,intent)
+//            enqueueWork(context,MyJobIntentService::class.java, JOB_ID,intent)
         }
 
+    }
+
+    override fun onStartCommand(intent: Intent?, flags: Int, startId: Int): Int {
+        command = intent?.getStringExtra(COMMAND).toString()
+        if (intent != null) {
+            enqueueWork(this, this::class.java, 101, intent)
+        }
+        return super.onStartCommand(intent, flags, startId)
     }
 
     override fun onHandleWork(intent: Intent) {
         Log.d(TAG, "onHandleWork: Mulai.....")
         val duration = intent.getLongExtra(EXTRA_DURATION, 0)
+
+        val intent = Intent(COMMAND_INTENT)
+        if (command=="ZHU LI , DO THE THINGS"){
+            intent.putExtra(BROADCAST_COMMAND_STATUS,"Your Command is : $command , my Response : Yes Sir !")
+        }else{
+            intent.putExtra(BROADCAST_COMMAND_STATUS,"Your Command is : $command , my Response : Is That You ?")
+        }
+        sendBroadcast(intent)
+
         try {
             Thread.sleep(duration)
             Log.d(TAG, "onHandleWork: Selesai.....")
+
         } catch (e: InterruptedException) {
             e.printStackTrace()
             Thread.currentThread().interrupt()
@@ -50,8 +59,6 @@ class MyJobIntentService : JobIntentService() {
         super.onDestroy()
         Log.d(TAG, "onDestroy()")
     }
-
-
 
 
 }
